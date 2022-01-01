3fe88c0622caa049a29a46f04f0142f4743d33bd
fshlarf <faishal.arif218@gmail.com>
2022-01-01 14:44:24 +0700

    add some change

---
+++ components/Button.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <button
+      v-on:click="clickMe"
+      class="px-6 py-4 bg-blue-400 border rounded text-white"
+    >
+      PRESS ME
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    clickMe() {},
+  },
+};
+</script>
+
+<style></style>
---
+++ pages/about.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>HLLO FROM ABOUt</h1>
+    <img src="/images/test.png" alt="Ga nemu" />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
